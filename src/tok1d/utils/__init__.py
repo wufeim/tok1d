976643e5f56ade6834c5d7c5770d1f87de490ef3
Wufei Ma <wufeim@gmail.com>
2025-07-06 18:25:06 -0400

    add example_images

---
+++ src/tok1d/utils/__init__.py
@@ -0,0 +1,4 @@
+example_images = [
+    'https://raw.githubusercontent.com/bytedance/1d-tokenizer/refs/heads/main/assets/ILSVRC2012_val_00008636.png',
+    'https://raw.githubusercontent.com/bytedance/1d-tokenizer/refs/heads/main/assets/ILSVRC2012_val_00010240.png',
+]
